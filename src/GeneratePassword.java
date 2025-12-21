75ed3fea46b79806f13d9083ead82c66744172bd
DzakiShofianHizbullah <jakishofian@gmail.com>
2025-12-21 19:41:04 +0700

    Initial commit - SecurePass Manager

--- src/GeneratePassword.java
+++ src/GeneratePassword.java
@@ -1,56 +1,52 @@
 import javax.swing.*;
+import java.awt.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class GeneratePassword extends JFrame {
 
-    JTextField akunField, lengthField, resultField;
-    JCheckBox upper, lower, number, symbol;
+    private JTextField akunField;
+    private JTextField lengthField;
+    private JTextField resultField;
+
+    private JCheckBox upper, lower, number, symbol;
+    private ArrayList<PasswordData> list;
 
     public GeneratePassword() {
+        list = FileUtil.loadData();
+
         setTitle("Generate Password");
-        setSize(420, 380);
-        setLayout(null);
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setLayout(new GridLayout(6, 2, 5, 5));
 
-        JLabel akunLabel = new JLabel("Nama Akun / Layanan");
-        JLabel lengthLabel = new JLabel("Panjang Password (Jumlah Karakter)");
-        JLabel resultLabel = new JLabel("Hasil Password");
-
-        akunLabel.setBounds(20,20,250,25);
-        lengthLabel.setBounds(20,60,300,25);
-        resultLabel.setBounds(20,190,200,25);
-
+        add(new JLabel("Nama Akun"));
         akunField = new JTextField();
+        add(akunField);
+
+        add(new JLabel("Panjang Password"));
         lengthField = new JTextField();
+        add(lengthField);
+
+        upper = new JCheckBox("Huruf Besar", true);
+        lower = new JCheckBox("Huruf Kecil", true);
+        number = new JCheckBox("Angka");
+        symbol = new JCheckBox("Simbol");
+
+        add(upper);
+        add(lower);
+        add(number);
+        add(symbol);
+
         resultField = new JTextField();
         resultField.setEditable(false);
-
-        akunField.setBounds(20,40,360,25);
-        lengthField.setBounds(20,80,360,25);
-        resultField.setBounds(20,210,360,25);
-
-        upper = new JCheckBox("Huruf Besar (A-Z)");
-        lower = new JCheckBox("Huruf Kecil (a-z)");
-        number = new JCheckBox("Angka (0-9)");
-        symbol = new JCheckBox("Simbol (!@#$)");
-
-        upper.setBounds(20,110,180,25);
-        lower.setBounds(200,110,180,25);
-        number.setBounds(20,140,180,25);
-        symbol.setBounds(200,140,180,25);
+        add(resultField);
 
         JButton genBtn = new JButton("Generate");
         JButton saveBtn = new JButton("Simpan");
 
-        genBtn.setBounds(80,260,110,30);
-        saveBtn.setBounds(220,260,110,30);
-
-        add(akunLabel); add(lengthLabel); add(resultLabel);
-        add(akunField); add(lengthField); add(resultField);
-        add(upper); add(lower); add(number); add(symbol);
-        add(genBtn); add(saveBtn);
+        add(genBtn);
+        add(saveBtn);
 
         genBtn.addActionListener(e -> generate());
         saveBtn.addActionListener(e -> save());
@@ -60,26 +56,12 @@ public class GeneratePassword extends JFrame {
 
     private void generate() {
         try {
-            int len = Integer.parseInt(lengthField.getText());
-            if (len < 6) throw new Exception();
-
-            String chars = "";
-            if (upper.isSelected()) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            if (lower.isSelected()) chars += "abcdefghijklmnopqrstuvwxyz";
-            if (number.isSelected()) chars += "0123456789";
-            if (symbol.isSelected()) chars += "!@#$%^&*";
-
-            if (chars.isEmpty()) throw new Exception();
-
-            Random r = new Random();
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < len; i++)
-                sb.append(chars.charAt(r.nextInt(chars.length())));
-
-            resultField.setText(sb.toString());
-
+            int length = Integer.parseInt(lengthField.getText());
+            resultField.setText(SecurityUtil.generate(length,
+                    upper.isSelected(), lower.isSelected(),
+                    number.isSelected(), symbol.isSelected()));
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(this, "Input tidak valid!");
+            JOptionPane.showMessageDialog(this, "Panjang harus angka!");
         }
     }
 
@@ -89,17 +71,16 @@ public class GeneratePassword extends JFrame {
             return;
         }
 
-        ArrayList<PasswordData> list = FileUtil.readData();
-        int id = list.size() + 1;
-
+        int id = FileUtil.getNextId(list);
         list.add(new PasswordData(
                 id,
                 akunField.getText(),
-                SecurityUtil.encrypt(resultField.getText()),
+                resultField.getText(),
                 LocalDate.now()
         ));
 
-        FileUtil.writeData(list);
-        JOptionPane.showMessageDialog(this, "Data tersimpan!");
+        FileUtil.saveData(list);
+        JOptionPane.showMessageDialog(this, "Password tersimpan!");
+        dispose();
     }
 }
--- src/SecurityUtil.java
+++ src/SecurityUtil.java
@@ -1,11 +1,25 @@
-import java.util.Base64;
+import java.security.SecureRandom;
 
 public class SecurityUtil {
-    public static String encrypt(String text) {
-        return Base64.getEncoder().encodeToString(text.getBytes());
-    }
 
-    public static String decrypt(String text) {
-        return new String(Base64.getDecoder().decode(text));
+    private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String NUMBER = "0123456789";
+    private static final String SYMBOL = "!@#$%^&*()-_=+[]{}<>?/";
+
+    public static String generate(int length, boolean upper, boolean lower, boolean number, boolean symbol) {
+        String chars = "";
+        if (upper) chars += UPPER;
+        if (lower) chars += LOWER;
+        if (number) chars += NUMBER;
+        if (symbol) chars += SYMBOL;
+
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            sb.append(chars.charAt(random.nextInt(chars.length())));
+        }
+        return sb.toString();
     }
 }
